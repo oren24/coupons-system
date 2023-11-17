fabd3ac44aed45c0fd8ee06f73cce500de97eac8
Oren Drori <oren.dro@gmail.com>
2023-11-17 19:08:33 +0200

    main

--- frontend/coupons-react/src/components/containers/FilterContainer/FilterContainer.tsx
+++ frontend/coupons-react/src/components/containers/FilterContainer/FilterContainer.tsx
@@ -11,29 +11,11 @@ import {CategoryContainer} from "../CategoryContainer/CategoryContainer";
 import {fetchAllCoupons} from "../CouponsContainer/CouponsContainer";
 
 // fetches all categories from the server
-export async function fetchAllCategories() {
-    try {
-        const response = await axios.get("http://localhost:8080/categories");
-        let data = response.data;
-        let categories: ICategory[] = data;
-        store.dispatch({ type: ActionType.FETCH_ALL_CATEGORIES, payload: categories });// this is the initial call to fetch coupons
-        return categories;
 
-    } catch (error: any) {
-        console.log(error.message);
-    }
-}
 
 export function FilterContainer() {
     const dispatch = useDispatch();
 
-    useEffect(() => {
-        fetchAllCategories();
-    }, []);
-
-
-    let categories = useSelector((state: AppState) => state.categories);
-// sets a filter function to view only the coupons from the selected categories that are not null
 
     function onResetClicked() {
         dispatch({type: ActionType.FETCH_ALL_COUPONS, payload: store.getState().allCoupons})
@@ -42,6 +24,7 @@ export function FilterContainer() {
     return (
         <div className={"FilterContainer"}>
             <CategoryContainer/>
+
         <button onClick={onResetClicked}>Reset filters</button>
 
         </div>
